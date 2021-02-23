1e23c1f7940536fd08f638f12b70743ee526cfdb
eliasfilipec8 <eliasfilipec8@gmail.com>
2021-02-23 17:28:31 -0300

    Add project files.

---
+++ VotingSystem/Controllers/APISystemVoting.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace VotingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APISystemVoting : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"API Sytem Voting On-line [{DateTime.UtcNow}]";
+        }
+
+        public List<string> GetUsers()
+        {
+            return new List<string>();
+        }
+    }
+}
